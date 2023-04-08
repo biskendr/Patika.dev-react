ee77e20f0b336e67cfd7ea72a68aa19233cf4c08
Burak İskender <burak.iskendr@gmail.com>
2023-04-08 17:49:40 +0300

    :bug: fix function name in function call.
    Fixed by changing the function called getGeoCoords to getCityByGeoCoords.

--- react-odev-02/src/pages/api/geo.js
+++ react-odev-02/src/pages/api/geo.js
@@ -1,11 +1,11 @@
-import { getGeoCoords } from "@/services/weatherServices"
+import { getCityByGeoCoords } from "@/services/weatherServices"
 
 //function handle http requests for location data
 export default async function handler(req, res) {
     //get query parameter from request object
     const { lat, lon } = req.query
     try {
-        const currentData = await getGeoCoords(lat, lon);
+        const currentData = await getCityByGeoCoords(lat, lon);
         res.status(200).json(currentData)
     } catch (error) {
         const cod = error.response?.data?.cod || "Unknown";
