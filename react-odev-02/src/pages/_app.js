7e82ffbcfe151da6fc6318ab53e118d5f48996a8
Burak İskender <burak.iskendr@gmail.com>
2023-04-10 23:49:45 +0300

    :sparkles: wrap app component with ThemeProvider and WeatherProvider.
    This commit adds the ThemeProvider and WeatherProvider components as wrappers for the App component.

--- react-odev-02/src/pages/_app.js
+++ react-odev-02/src/pages/_app.js
@@ -1,5 +1,12 @@
+import { ThemeProvider } from '@/context/ThemeContext'
+import { WeatherProvider } from '@/context/WeatherContext'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //wrap app with the ThemeProvider and WeatherProvider
+  return <ThemeProvider>
+    <WeatherProvider>
+      <Component {...pageProps} />
+    </WeatherProvider>
+  </ThemeProvider>
 }
