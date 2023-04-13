69a5c232c153418ba1614ff81118ea795058d70e
Burak İskender <burak.iskendr@gmail.com>
2023-04-13 23:22:02 +0300

    :recycle: refactor geoCoords to handle geolocation permissions and provide default coordinates for errors.

--- react-odev-02/src/helpers/getGeoCoords.js
+++ react-odev-02/src/helpers/getGeoCoords.js
@@ -2,17 +2,50 @@
 export const geoCoords = () => {
     return new Promise((resolve, reject) => {
         //if browser supports geolocation, call getCurrentPosition on navigator.geolocation.
-        if (window.navigator.geolocation) {
+        if (window.navigator.permissions) {
+            //query for geolocation permission
+            window.navigator.permissions.query({ name: 'geolocation' }).then((permission) => {
+                //if permission is granted, get the current position
+                if (permission.state === 'granted') {
+                    window.navigator.geolocation.getCurrentPosition(
+                        ({ coords }) => {
+                            resolve(coords);
+                        },
+                        //if there is an error, return default coordinates
+                        (err) => {
+                            resolve({ latitude: 41.0091982, longitude: 28.9662187 })
+                        }
+                    );
+                    //if permission is prompt, ask for permission and get current position
+                } else if (permission.state === 'prompt') {
+                    window.navigator.geolocation.getCurrentPosition(
+                        ({ coords }) => {
+                            resolve(coords);
+                        },
+                        //if there is an error, return default coordinates
+                        (err) => {
+                            resolve({ latitude: 41.0091982, longitude: 28.9662187 })
+                        }
+                    );
+                    //if permission is denied, return default coordinates
+                } else if (permission.state === 'denied') {
+                    resolve({ latitude: 41.0091982, longitude: 28.9662187 })
+                }
+            });
+            //if permission is not available, try to get current position
+        } else if (window.navigator.geolocation) {
             window.navigator.geolocation.getCurrentPosition(
                 ({ coords }) => {
                     resolve(coords);
                 },
+                //if there is an error, return default coordinates
                 (err) => {
-                    reject(err);
+                    resolve({ latitude: 41.0091982, longitude: 28.9662187 })
                 }
             );
+            //if geolocation is not supported, log an error message
         } else {
-            console.log("Geo Location not supported");
+            console.log('Geo Location not supported');
         }
     });
-}
+};
